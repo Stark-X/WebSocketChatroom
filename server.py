b290d214fa9f5c3031c84f09e663c7a7de64a48e
Stark-X <13653522+Stark-X@users.noreply.github.com>
2018-05-03 17:03:07 +0800

    Enable websocket ping frame

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from server.chatroom import ChatRoomHandler
 if __name__ == '__main__':
     app = web.Application([
         (r"/", ChatRoomHandler),
-    ])
+    ], websocket_ping_interval=3)
 
     options.parse_config_file("./config/logger.conf")
     app.listen(8080)
